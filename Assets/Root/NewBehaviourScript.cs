3fbcf935142304a5531a83f1b9332aa947015ccb
yenmoc <phongsoyenmoc.diep@gmail.com>
2020-03-01 19:52:19 +0700

    add main scene

--- Assets/Root/NewBehaviourScript.cs
+++ Assets/Root/NewBehaviourScript.cs
@@ -1,18 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Start!!!!");
     }
 }
